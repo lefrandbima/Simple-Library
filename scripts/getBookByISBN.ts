a59cf225d80002d9d748f27d31f7346b70855e9b
lefrandbima <lefrandbima@gmail.com>
2023-10-07 16:02:08 +0700

    function executioons

---
+++ scripts/getBookByISBN.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+import { SimpleLibrary } from "../typechain";
+
+async function main() {
+    // contract instance
+    const simpleLibrary = await ethers.getContract<SimpleLibrary>("SimpleLibrary");
+
+    const getBook = await simpleLibrary.getBookByISBN('123hsher');
+    console.log('Detail buku :', getBook);
+}
+
+main().catch((err) => {
+    console.log('error =', err);
+    process.exitCode = 1;
+});
